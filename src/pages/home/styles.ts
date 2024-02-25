b76efa5587006ed7ad889bf404673525e658b1d3
Italo <italodopeey@gmail.com>
2024-02-25 13:10:08 -0300

    feat: ensuring home styling

--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,7 +1,35 @@
-import { styled } from '@ignite-ui/react'
+import { styled, Heading, Text } from '@ignite-ui/react'
 
-export const Container = styled('div', {})
+export const Container = styled('div', {
+  maxWidth: 'calc(100vw - (100vw - 1160px) / 2)',
+  marginLeft: 'auto',
+  height: '100vh',
+  display: 'flex',
+  alignItems: 'center',
+  gap: '$20',
+})
 
-export const Hero = styled('div', {})
+export const Hero = styled('div', {
+  maxWidth: 480,
+  padding: '0 $10',
 
-export const Preview = styled('div', {})
+  [`> ${Heading}`]: {
+    '@media(max-width: 600px)': {
+      fontSize: '$6xl',
+    },
+  },
+
+  [`> ${Text}`]: {
+    marginTop: 2,
+    color: '$gray200',
+  },
+})
+
+export const Preview = styled('div', {
+  paddingRight: '$8',
+  overflow: 'hidden',
+
+  '@media(max-width: 600px)': {
+    display: 'none',
+  },
+})
