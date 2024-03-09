1f71915d9f5f0ec54121858024f43ada4229ccf7
Italo <italodopeey@gmail.com>
2024-03-08 22:12:33 -0300

    feat: Added TimeIntervals component to allow users to define their available time intervals for each day of the week.
    - Integrated with Zod for schema validation.
    
    - Displayed form errors for improved user experience.
    
    Future tasks:
    
    [ ] - Ensuring form submission is included (handleSet Time Intervals), awaiting backend integration.

---
+++ src/pages/register/time-intervals/index.page.tsx
@@ -0,0 +1,120 @@
+import {
+  Button,
+  Checkbox,
+  Heading,
+  MultiStep,
+  Text,
+  TextInput,
+} from '@ignite-ui/react'
+import { Container, Header } from '../styles'
+import * as S from './styles'
+import { ArrowRight } from 'phosphor-react'
+import { Controller, useFieldArray, useForm } from 'react-hook-form'
+import { getWeekDays } from '../../../utils/get-week-days'
+import {
+  TimeIntervalsFormData,
+  timeIntervalsFormSchema,
+} from '../../../@zod-schemas/time-intervals-form-schema'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+export default function TimeIntervals() {
+  const {
+    register,
+    handleSubmit,
+    control,
+    watch,
+    formState: { isSubmitting, errors },
+  } = useForm({
+    resolver: zodResolver(timeIntervalsFormSchema),
+    defaultValues: {
+      intervals: [
+        { weekDay: 0, enabled: false, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 1, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 2, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 3, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 4, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 5, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 6, enabled: false, startTime: '08:00', endTime: '18:00' },
+      ],
+    },
+  })
+
+  const { fields } = useFieldArray({
+    control,
+    name: 'intervals',
+  })
+
+  const intervals = watch('intervals')
+
+  const weekDays = getWeekDays()
+
+  async function handleSetTimeIntervals({ intervals }: TimeIntervalsFormData) {
+    console.log(intervals)
+  }
+
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Quase lá</Heading>
+        <Text>
+          Defina o intervalo de horários que você está disponível em cada dia da
+          semana.
+        </Text>
+
+        <MultiStep size={4} currentStep={3} />
+      </Header>
+
+      <S.IntervalBox onSubmit={handleSubmit(handleSetTimeIntervals)} as="form">
+        <S.IntervalsContainer>
+          {fields.map((field, index) => (
+            <S.IntervalItem key={field.id}>
+              <S.IntervalDay>
+                <Controller
+                  name={`intervals.${index}.enabled`}
+                  control={control}
+                  render={({ field }) => {
+                    return (
+                      <Checkbox
+                        onCheckedChange={(checked) => {
+                          field.onChange(checked === true)
+                        }}
+                        checked={field.value}
+                      />
+                    )
+                  }}
+                />
+                <Text>{weekDays[field.weekDay]}</Text>
+              </S.IntervalDay>
+
+              <S.IntervalInput>
+                <TextInput
+                  size="sm"
+                  type="time"
+                  step={60}
+                  disabled={intervals[index].enabled === false}
+                  {...register(`intervals.${index}.startTime`)}
+                />
+                <TextInput
+                  size="sm"
+                  type="time"
+                  step={60}
+                  disabled={intervals[index].enabled === false}
+                  {...register(`intervals.${index}.endTime`)}
+                />
+              </S.IntervalInput>
+            </S.IntervalItem>
+          ))}
+        </S.IntervalsContainer>
+
+        {errors.intervals?.root && (
+          <S.FormError size="sm">{errors.intervals.root.message}</S.FormError>
+        )}
+
+        <Button type="submit" disabled={isSubmitting}>
+          Próximo passo
+          <ArrowRight />
+        </Button>
+      </S.IntervalBox>
+    </Container>
+  )
+}
