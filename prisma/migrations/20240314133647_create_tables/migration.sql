7732edd04a823866e7c81a03c736c2c58737b40a
Italo <italodopeey@gmail.com>
2024-03-14 10:45:00 -0300

    chore: re-create schema

--- prisma/migrations/20240314133647_create_tables/migration.sql
+++ prisma/migrations/20240314133647_create_tables/migration.sql
@@ -3,6 +3,7 @@ CREATE TABLE "users" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "username" TEXT NOT NULL,
     "name" TEXT NOT NULL,
+    "bio" TEXT,
     "email" TEXT,
     "avatar_url" TEXT,
     "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
